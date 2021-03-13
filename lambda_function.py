7a5d4e61b5a71161c477c76a3dcdbf52795aac6b
Rajesh Kanna <75661804+rajeshias@users.noreply.github.com>
2021-03-13 20:15:59 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,44 +1,36 @@
 from s3_textract_functions import *
-import codecs
-import json, boto3
+
+import boto3
 from pprint import pprint
 
-
+def datachunk(x):
+    """Comprehend accepts textfile upto 5000 bytes only. More than that we need to chunk it into a list of textfiles"""
+    y=[]
+    while x:
+        y.append(str(x[:5000]))
+        x=x[5000:]
+    return y
+    
 def lambda_handler(event, context):
-    bucketName = 'cvreader'
-    fileName = 'vijaymamacv.pdf'
+    
+    bucketName=''
+    fileName=''
     jobId = startJob(bucketName, fileName)
     print(f"Started job with id: {jobId}")
-    response = getJobResults(jobId)
-
+    response=getJobResults(jobId)
+    
     # Writing Textract Output to Text:
-    textFile = ''
+    textFile=''
     for item in response["Blocks"]:
         if item["BlockType"] == "LINE":
-            textFile += item["Text"] + ' '
-
-    comp = boto3.client('comprehend')
-
-    entitty = comp.detect_entities(Text=textFile, LanguageCode='en')
-
-    # batchkp=comp.batch_detect_key_phrases(TextList=datachunker(para),LanguageCode='en') //batch_detect_entities, batch_detect_sentiment, etc
-    # keyp=comp.detect_key_phrases(Text=textFile, LanguageCode='en')
-    # res=comp.detect_sentiment(Text=para, LanguageCode='en')
-
-    # s3=boto3.client("s3").get_object(Bucket="redditwritingpromptssample", Key="Textcomprehend.txt")
-    # para=str(s3['Body'].read())
-    # comp=boto3.client('comprehend')
-    # res=comp.detect_sentiment(Text=para, LanguageCode='en')
-    # entitty=comp.detect_entities(Text=para, LanguageCode='en')
-    # batchkp=comp.batch_detect_key_phrases(TextList=datachunker(para),LanguageCode='en') //batch_detect_entities, batch_detect_sentiment, etc
-    # keyp=comp.detect_key_phrases(Text=para, LanguageCode='en')
-
-    for i in entitty['Entities']:
-        if i['Type'] == 'TITLE':
-            print(i['Text'])
-
-    # pprint(keyp)
-    # print(res)
-    # pprint(batchkp)
-
+            textFile+=item["Text"]+' '
+            
+    comp=boto3.client('comprehend')
+    entitty=comp.batch_detect_entities(TextList=datachunk(textFile), LanguageCode='en')
+    
+    for i in entitty['ResultList']:
+        for j in i['Entities']:
+            if j['Type']=='ORGANIZATION':
+                print(j['Text'])
+   
     return "done"
